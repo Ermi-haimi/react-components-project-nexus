ef411fbb79de2747cdb3b9d77fcd3fbc3329a9e6
Ermias Haimanot <ermiashtsegaye@gmail.com>
2025-09-23 23:22:21 +0300

    added the components to the main file

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Header from './header.jsx'
+import StatCards from './StatCards.jsx'
+import Projects from './Projects.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Header />
+    <StatCards/>
+    <Projects />
   </StrictMode>,
 )
