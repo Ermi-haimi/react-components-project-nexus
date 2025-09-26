87fc513a297e7238aeeb065516a976fd64d1ccc8
Ermias Haimanot <ermiashtsegaye@gmail.com>
2025-09-26 14:27:58 +0300

    fix a typo

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Header from './header.jsx'
+import Header from './Header.jsx'
 import StatCards from './StatCards.jsx'
 import Projects from './Projects.jsx'
 import RecentTasks from './RecentTasks.jsx'
 import Team from './Team.jsx'
 import Notifications from './Notifications.jsx'
 import RecentActivities from './RecentActivities.jsx'
+import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -20,5 +21,6 @@ createRoot(document.getElementById('root')).render(
     <Team />
     <Notifications />
     <RecentActivities />
+    <Footer />
   </StrictMode>,
 )
